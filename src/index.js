11ca46f37ef638e27c100deeae944e8104f63a3c
leolice36 <leoliceanaelmojica@gmail.ccom>
2025-05-12 05:10:23 +0800

    Add date inputs to Task details

--- src/index.js
+++ src/index.js
@@ -5,13 +5,39 @@ import {generateDefaults} from './modules/coreFunction/defaultObjs'
 import {addProject, editProject, addTask, editTask, addTag, editTag,tagUrgency,tagOther} from "./modules/coreFunction/basicFunction"
 import filter from './modules/coreFunction/filter'
 import { subDays, addDays, formatISO, parseISO, isToday, isTomorrow, isThisWeek, isThisMonth, isPast } from 'date-fns';
-
 import htmlGenerator from "./modules/htmlGenerator"
 generateDefaults()
 htmlGenerator.generateTagSectionInTask(registry.allTags,'task-1')
 htmlGenerator.generateTagSectionInFIlter(registry.allTags)
 htmlGenerator.generateProjectList(registry.allProjects)
 htmlGenerator.generateTaskList(registry.allTasks)
+
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.css'; // Import the default CSS for styling
+import { format } from "date-fns"
+// Initialize Flatpickr
+flatpickr('#do-date', {
+  dateFormat: 'Y-m-d',
+  altInput: true,   
+  altFormat: "F j, Y", 
+  onChange: function(selectedDates, dateStr, instance) {
+    const selectedDate = new Date(dateStr)
+    console.log(selectedDate.toISOString());
+    console.log(format(selectedDate, 'MMMM d, yyyy'));
+  },
+});
+flatpickr('#due-date', {
+	dateFormat: 'Y-m-d',
+	altInput: true,   
+	altFormat: "F j, Y", 
+	onChange: function(selectedDates, dateStr, instance) {
+	  const selectedDate = new Date(dateStr)
+	  console.log(selectedDate.toISOString());
+	  console.log(format(selectedDate, 'MMMM d, yyyy'));
+	},
+  });
+
 // const randomTask1 = registry.allTasks[0]
 // const randomTask2 = registry.allTasks[1]
 // const randomTask3 = registry.allTasks[2]
--- src/sandbox/index.js
+++ src/sandbox/index.js
@@ -5,10 +5,38 @@ import {removeTag, addProject, editProject, addTask, editTask, addTag, editTag,t
 import testScript from './test'
 import { parseISO, isToday, isTomorrow, isThisWeek, isThisMonth, isPast } from 'date-fns';
 // generateDefaults()
-import htmlGenerator from "../modules/htmlGenerator"
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.css'; // Import the default CSS for styling
+import { format } from "date-fns"
+// Initialize Flatpickr
+flatpickr('#datepicker', {
+  dateFormat: 'Y-m-d', // Set the output format to ISO
+  onChange: function(selectedDates, dateStr, instance) {
+    const selectedDate = new Date(dateStr)
+    console.log(selectedDate.toISOString());
+    console.log(format(selectedDate, 'MMMM d, yyyy'));
+  },
+});
+flatpickr('#datepickerrange', {
+    mode: 'range',
+    dateFormat: 'Y-m-d',
+    onChange: function(selectedDates) {
+      const [start, end] = selectedDates;
+  
+      if (start) {
+        console.log('Start:', format(start, 'MMMM d, yyyy'));
+        console.log('Start:', start.toISOString());
+      }
+  
+      if (end) {
+        console.log('Start:', format(end, 'MMMM d, yyyy'));
+        console.log('End:', end.toISOString());
+      }
+    },
+  });
+  
 
 
-htmlGenerator.generateTagSectionInTask(registry.allTags,'task-1')
 
 
 
