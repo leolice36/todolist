6bb33350e2bfa893cf45620a23df284e36e9adb1
leolice36 <leoliceanaelmojica@gmail.ccom>
2025-06-06 03:10:43 +0800

    complete project list CSS

--- src/modules/htmlGenerator.js
+++ src/modules/htmlGenerator.js
@@ -100,8 +100,8 @@ function generateProjectList(projectsArr){
         const projectDiv = document.createElement('div')
         projEntryContainer.classList.add('entry-container')
         svgContainer.classList.add('svg-container')
-        svgContainer.innerHTML = `<svg width="5" height="5" viewBox="0 0 5 5" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="2.5" cy="2.5" r="2.5" fill="#512EAB"/>
+        svgContainer.innerHTML = `<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="3" cy="3" r="3" fill="#512EAB"/>
           </svg>`
 
         projectDiv.textContent = project.name
